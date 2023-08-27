c6f3f2717c1a129a2c3b834b4b7e5d6cd7ff560b
RyukiSasaki <ryuki.sasaki@sasatech.co.jp>
2023-08-27 16:15:34 +0900

    add code

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
+webhook = WebhookClient(os.getenv('SLACK_URL'))
+
+
 
 client = HTTP(testnet=False, api_key=os.getenv('BYBIT_API_KEY'), api_secret=os.getenv('BYBIT_API_SECRET'))
-
-# ティッカーを取得
 tickers = client.get_tickers(category="inverse")
 
 # 配列の中身を表示
 for ticker in tickers['result']['list']:
-    print("銘柄名: " + ticker['symbol'])
-    print("現在価格: $" + str(ticker['lastPrice']))
-    print("24時間出来高: $" + str(ticker['volume24h']))
-    print("---")
+    response = webhook.send(text="銘柄名: " + ticker['symbol'] + "\n現在価格: $" + str(ticker['lastPrice']) + "\n24時間出来高: $" + str(ticker['volume24h']))
